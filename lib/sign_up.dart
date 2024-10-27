b4ff1910ef9e6aa290976110e6ccd2861d98c500
Rishabh Srivastava <rishabhsrivastava918@gmail.com>
2024-10-27 23:55:18 +0530

    Initial commit

---
+++ lib/sign_up.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Sign_up extends StatelessWidget {
+  const Sign_up({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const  Scaffold(
+      body: Center(
+
+        child: Text("Sign up Page", style: TextStyle(fontSize: 35),),
+
+
+        
+        
+      ),
+    );
+  }
+}
